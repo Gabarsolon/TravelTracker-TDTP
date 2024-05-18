4b07eff3ec7179d6f378c9d5dd53ad501f2c3ff9
917tapoimarius <marius.tapoi@stud.ubbcluj.ro>
2024-05-18 16:26:45 +0300

    AutomatedLoginFeature

---
+++ TapoiMarius-Stefan_SerenityJUnit/src/test/java/org/example/pages/HomePage.java
@@ -0,0 +1,29 @@
+package org.example.pages;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("http://localhost:5173/")
+public class HomePage extends PageObject {
+
+    public void login(String username, String password) {
+        // Find the username and password input fields
+        WebElementFacade usernameField = find(By.id("username"));
+        WebElementFacade passwordField = find(By.id("password"));
+
+        // Enter the username and password
+        usernameField.type(username);
+        passwordField.type(password);
+
+        // Submit the login form
+        WebElementFacade submitButton = find(By.className("button1"));
+        submitButton.click();
+    }
+
+    public WebElementFacade getLoggedInUser() {
+        return find(By.className("user-id"));
+    }
+}
---
+++ TapoiMarius-Stefan_SerenityJUnit/src/test/java/org/example/steps/serenity/EndUserSteps.java
@@ -0,0 +1,29 @@
+package org.example.steps.serenity;
+
+import org.example.pages.HomePage;
+import net.thucydides.core.annotations.Step;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+public class EndUserSteps {
+
+    HomePage homePage;
+
+
+    @Step
+    public void is_the_home_page() {
+        homePage.open();
+    }
+
+    @Step
+    public void logs_in(String username, String password) {
+        homePage.login(username, password);
+    }
+
+    @Step
+    public void should_be_logged_in(String username) {
+        assertThat(homePage.getLoggedInUser().getText(), containsString(username));
+    }
+}
